1f573df6b49ca214562a481f9839a6242710e7aa
JotaveOliveira <jv.oa202@gmail.com>
2020-01-04 13:06:53 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/IHelp/Application/IHelpApplication.java
@@ -0,0 +1,13 @@
+package br.com.IHelp.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IHelpApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/IHelp/Entity/Usuario.java
@@ -0,0 +1,16 @@
+package br.com.IHelp.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Usuario {
+
+	private String nome;
+	private String cpf;
+	private String telefone;
+	
+}
