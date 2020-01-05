85c7c5291da349c57be6e253178df1c73985172d
JotaveOliveira <jv.oa202@gmail.com>
2020-01-05 08:23:10 -0300

    Refatorando o projeto

--- src/main/java/br/com/IHelp/domain/Entities/Usuario.java
+++ src/main/java/br/com/IHelp/domain/Entities/Usuario.java
@@ -1,16 +1,19 @@
-package br.com.IHelp.Entity;
+package br.com.IHelp.domain.Entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
 	private String nome;
 	private String cpf;
 	private String telefone;
+	private String estado;
 	
 }
